cbd39633ae5f53bf429ec3b0c2787a8b7933e37b
awildberg <77085223+awildberg@users.noreply.github.com>
2021-10-14 22:45:43 +0200

    Update binomial_distribution.cr

--- lib/binomial_distribution.cr
+++ lib/binomial_distribution.cr
@@ -1,4 +1,4 @@
-require "probability_distribution"
+require "../lib/probability_distribution"
 # This class provides an object for encapsulating binomial distributions
 # Ported to Ruby from PHPMath class by Bryan Donovan
 # Author:: Mark Hale
